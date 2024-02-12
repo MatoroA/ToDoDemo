35cdbc8459bb7d18ffbcee67498b9d3f95024937
Andani Matoro <andanimatoro@gmail.com>
2024-02-12 23:32:11 +0200

    first commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Andani Matoro on 2024/02/12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
